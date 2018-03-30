0bf999d93627b42b62f48496269b466545606fcd
joo yun-whoi <yunhoi129@gmail.com>
2018-03-30 20:04:22 +0900

    ios setting

--- ios/RNKakaoLink.m
+++ ios/RNKakaoLink.m
@@ -1,5 +1,6 @@
 
 #import "RNKakaoLink.h"
+#import <KakaoLink/KakaoLink.h>
 
 @implementation RNKakaoLink
 
@@ -9,5 +10,56 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_REMAP_METHOD(link,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    KMTTemplate *template = [KMTFeedTemplate feedTemplateWithBuilderBlock:^(KMTFeedTemplateBuilder * _Nonnull feedTemplateBuilder) {
+        
+        // 콘텐츠
+        feedTemplateBuilder.content = [KMTContentObject contentObjectWithBuilderBlock:^(KMTContentBuilder * _Nonnull contentBuilder) {
+            contentBuilder.title = @"디저트 사진";
+            contentBuilder.desc = @"아메리카노, 빵, 케익";
+            contentBuilder.imageURL = [NSURL URLWithString:@"http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg"];
+            contentBuilder.link = [KMTLinkObject linkObjectWithBuilderBlock:^(KMTLinkBuilder * _Nonnull linkBuilder) {
+                linkBuilder.mobileWebURL = [NSURL URLWithString:@"https://developers.kakao.com"];
+            }];
+        }];
+        
+        // 소셜
+        feedTemplateBuilder.social = [KMTSocialObject socialObjectWithBuilderBlock:^(KMTSocialBuilder * _Nonnull socialBuilder) {
+            socialBuilder.likeCount = @(10);
+            socialBuilder.commnentCount = @(20);
+            socialBuilder.sharedCount = @(30);
+            socialBuilder.viewCount = @(40);
+        }];
+        
+        // 버튼
+        [feedTemplateBuilder addButton:[KMTButtonObject buttonObjectWithBuilderBlock:^(KMTButtonBuilder * _Nonnull buttonBuilder) {
+            buttonBuilder.title = @"웹으로 이동";
+            buttonBuilder.link = [KMTLinkObject linkObjectWithBuilderBlock:^(KMTLinkBuilder * _Nonnull linkBuilder) {
+                linkBuilder.mobileWebURL = [NSURL URLWithString:@"https://developers.kakao.com"];
+            }];
+        }]];
+        [feedTemplateBuilder addButton:[KMTButtonObject buttonObjectWithBuilderBlock:^(KMTButtonBuilder * _Nonnull buttonBuilder) {
+            buttonBuilder.title = @"앱으로 이동";
+            buttonBuilder.link = [KMTLinkObject linkObjectWithBuilderBlock:^(KMTLinkBuilder * _Nonnull linkBuilder) {
+                linkBuilder.iosExecutionParams = @"param1=value1&param2=value2";
+                linkBuilder.androidExecutionParams = @"param1=value1&param2=value2";
+            }];
+        }]];
+    }];
+    
+    [[KLKTalkLinkCenter sharedCenter] sendDefaultWithTemplate:template success:^(NSDictionary<NSString *,NSString *> * _Nullable warningMsg, NSDictionary<NSString *,NSString *> * _Nullable argumentMsg) {
+        // 성공
+        resolver();
+    } failure:^(NSError * _Nonnull error) {
+        // 에러
+        reject();
+    }];
+    
+    
+}
+
 @end
-  
+
