21f785a1d89fb2974d70da568a0a947c3a355ae2
joo yun-whoi <yunhoi129@gmail.com>
2018-04-21 22:13:32 +0900

    iOS kakao navi 추가

--- ios/RNKakaoLink.m
+++ ios/RNKakaoLink.m
@@ -2,6 +2,7 @@
 #import "RNKakaoLink.h"
 #import <KakaoLink/KakaoLink.h>
 #import <KakaoMessageTemplate/KakaoMessageTemplate.h>
+#import <KakaoNavi/KakaoNavi.h>
 
 @implementation RNKakaoLink
 
@@ -15,39 +16,51 @@ RCT_EXPORT_METHOD(link: (NSDictionary *)params
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-    
     KMTTemplate *template = [KMTFeedTemplate feedTemplateWithBuilderBlock:^(KMTFeedTemplateBuilder * _Nonnull feedTemplateBuilder) {
         
         // 콘텐츠
         feedTemplateBuilder.content = [KMTContentObject contentObjectWithBuilderBlock:^(KMTContentBuilder * _Nonnull contentBuilder) {
             contentBuilder.title = [params objectForKey:@"title"];
             contentBuilder.desc = [params objectForKey:@"desc"];
-            contentBuilder.imageURL = [NSURL URLWithString:[params objectForKey:@"imageURL"]];
+            contentBuilder.imageURL = [NSURL URLWithString:[params objectForKey:@"imageUrl"]];
             contentBuilder.link = [KMTLinkObject linkObjectWithBuilderBlock:^(KMTLinkBuilder * _Nonnull linkBuilder) {
-                linkBuilder.mobileWebURL = [NSURL URLWithString:[params objectForKey:@"mobileWebURL"]];
-                linkBuilder.webURL = [NSURL URLWithString:[params objectForKey:@"mobileWebURL"]];
                 linkBuilder.iosExecutionParams = [params objectForKey:@"iosExecutionParams"];
                 linkBuilder.androidExecutionParams = [params objectForKey:@"androidExecutionParams"];
             }];
         }];
-        // 버튼
+        
         [feedTemplateBuilder addButton:[KMTButtonObject buttonObjectWithBuilderBlock:^(KMTButtonBuilder * _Nonnull buttonBuilder) {
             buttonBuilder.title = @"앱으로 이동";
             buttonBuilder.link = [KMTLinkObject linkObjectWithBuilderBlock:^(KMTLinkBuilder * _Nonnull linkBuilder) {
-                linkBuilder.iosExecutionParams = [params objectForKey:@"iosExecutionParams"];
-                linkBuilder.androidExecutionParams = [params objectForKey:@"androidExecutionParams"];
+                linkBuilder.iosExecutionParams = @"param1=value1&param2=value2";
+                linkBuilder.androidExecutionParams = @"param1=value1&param2=value2";
             }];
         }]];
     }];
     
     [[KLKTalkLinkCenter sharedCenter] sendDefaultWithTemplate:template success:^(NSDictionary<NSString *,NSString *> * _Nullable warningMsg, NSDictionary<NSString *,NSString *> * _Nullable argumentMsg) {
-        // 성공
-        NSDictionary *result = @{@"success" : @"true"};
-        resolve(result);
     } failure:^(NSError * _Nonnull error) {
-        // 에러
-        reject(@"FAIL", @"FAIL", error);
     }];
+}
+
+RCT_EXPORT_METHOD(navi: (NSDictionary *) naviParams)
+{
+    //name: _.get(data, 'name', ''),
+    //lat: lat,
+    //lng: lng
+    NSLog(@"%@", naviParams);
+    // 목적지 생성
+    KNVLocation *destination = [KNVLocation locationWithName:[naviParams objectForKey:@"name"]
+                                                           x:[NSNumber numberWithDouble:[[naviParams objectForKey:@"lng"] doubleValue]]
+                                                           y:[NSNumber numberWithDouble:[[naviParams objectForKey:@"lat"] doubleValue]]];
+    
+    // WGS84 좌표타입 옵션 설정
+    KNVOptions *options = [KNVOptions options];
+    options.coordType = KNVCoordTypeWGS84;
+    
+    // 목적지 공유 실행
+    KNVParams *params = [KNVParams paramWithDestination:destination options:options];
+    [[KNVNaviLauncher sharedLauncher] shareDestinationWithParams:params completion:nil];
 }
 
 @end
